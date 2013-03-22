e16354845d5b2fd387651d022faceb04b2121265
Torky Almohaisen <torky.almohaisen@hotmail.com>
2013-03-22 01:51:10 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+# TURKI: Updated some gems to be explicitly specified to not update.
+
+
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0.beta1'
+
+gem 'sqlite3', '1.3.7'
+
+group :assets do
+  gem 'sass-rails',   '4.0.0.beta1'
+  gem 'coffee-rails', '4.0.0.beta1'
+  gem 'uglifier', '1.0.3'
+end
+
+gem 'jquery-rails', '2.2.1'
+gem 'turbolinks', '1.0.0'
+gem 'jbuilder', '1.0.1'
+
+
+
+# To use debugger
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '8afe929971b99cd769738218c7cd8c9af6e10defcfbb15cff813d62087dd9ff22b288af3e80494597b9bfefdd6d7a7d3cc44a6a1ed54cd0addf567ae0861dd9b'
